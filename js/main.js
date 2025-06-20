5da1a47d23d80586fd3f12130ab972e6da3e98a5
pardue <pardue@users.noreply.github.com>
2025-06-20 01:43:27 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
